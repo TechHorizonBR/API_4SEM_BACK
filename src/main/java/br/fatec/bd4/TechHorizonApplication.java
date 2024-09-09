35d6dde5e87bff9ceb979beef738bf71fb1dc841
LuisPGuimaraes <luisegsilva.le@gmail.com>
2024-09-09 01:54:53 -0300

    improve: correct files to run

---
+++ src/main/java/br/fatec/bd4/TechHorizonApplication.java
@@ -0,0 +1,13 @@
+package br.fatec.bd4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechHorizonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechHorizonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/fatec/bd4/TechHorizonApplicationTests.java
@@ -0,0 +1,13 @@
+package br.fatec.bd4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechHorizonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
