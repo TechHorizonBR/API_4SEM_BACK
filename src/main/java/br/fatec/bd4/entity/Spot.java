c2fba2b18ebb822b45a8d393987c843fe89e55dd
Raul Neto <rjbnetto@gmail.com>
2024-09-11 22:27:29 -0300

    feat: Add entity Spot

--- src/main/java/br/fatec/bd4/entity/Spot.java
+++ src/main/java/br/fatec/bd4/entity/Spot.java
@@ -1,3 +1,43 @@
+package br.fatec.bd4.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Spot {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "createdAt", nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(name = "latitude", nullable = false, precision = 10)
+    private Double lat;
+
+    @Column(name = "longitude", nullable = false, precision = 12, scale = 10)
+    private Double lng;
+
+    @Column(name = "fullName", nullable = false)
+    private String fullName;
+
+    @Column(name = "device", nullable = false)
+    private String device;
+
+    @Column(name = "localName", nullable = false)
+    private String localName;
+
+    @Column(name = "macAdress", nullable = false)
+    private String macAdress;
+
 }
