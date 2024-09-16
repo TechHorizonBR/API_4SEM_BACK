781e3e62a129a9720fffb53c933d1ca6f464c687
santosjhony12 <santosdesouzajhony@gmail.com>
2024-09-16 20:07:26 -0300

    improve: Removing import not important

--- src/main/java/br/fatec/bd4/Filters/FilterType.java
+++ src/main/java/br/fatec/bd4/Filters/FilterType.java
@@ -7,13 +7,10 @@ public class FilterType {
     private Filter filter;
     private List<String> values;
 
-    public void setFilter(Filter filter){
-        this.filter = filter;
-    }
-
     public FilterType(Filter filter){
         this.filter = filter;
     }
+
     public List<String> getValues(){
         this.values = filter.getValues();
         return values;
