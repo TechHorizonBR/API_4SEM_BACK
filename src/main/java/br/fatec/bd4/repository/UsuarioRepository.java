1f35cfb4c4c91507770a683fe215b63e6e5bfc10
santosjhony12 <santosdesouzajhony@gmail.com>
2024-09-24 17:17:00 -0300

    refactor: removed pagination from filter endpoint

--- src/main/java/br/fatec/bd4/repository/UsuarioRepository.java
+++ src/main/java/br/fatec/bd4/repository/UsuarioRepository.java
@@ -1,18 +1,15 @@
 package br.fatec.bd4.repository;
 
 import br.fatec.bd4.entity.Usuario;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     @Query("SELECT u FROM Usuario u order by u.nome ASC")
-    Page<Usuario> findAllUsuarios(Pageable pageable);
+    List<Usuario> findAllUsuarios();
 
 }
