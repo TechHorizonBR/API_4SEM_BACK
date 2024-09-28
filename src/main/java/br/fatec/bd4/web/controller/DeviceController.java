5f59add7ae63ebe768ddabaf9defd90642e755ca
Kainanthyz <santos.blanzgamer@gmail.com>
2024-09-27 21:32:57 -0300

    docs:"updating documentation about devices and register"

--- src/main/java/br/fatec/bd4/web/controller/DeviceController.java
+++ src/main/java/br/fatec/bd4/web/controller/DeviceController.java
@@ -2,6 +2,11 @@ package br.fatec.bd4.web.controller;
 
 import br.fatec.bd4.entity.Device;
 import br.fatec.bd4.service.DeviceService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +22,18 @@ public class DeviceController {
     private DeviceService deviceService;
 
     // Retorna todos os dispositivos
+    @Operation(
+        summary = "Get all Devices.",
+        description = "Endpoint responsible for retrieving a list of all devices.",
+        responses = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Getting has been executed successfully.",
+                    content = @Content(mediaType = "application/json", schema = @Schema(
+                        implementation = Device.class))
+                    )
+            }
+        )        
     @GetMapping
     public ResponseEntity<List<Device>> getAllDevices() {
         List<Device> devices = deviceService.findAll();
@@ -24,6 +41,18 @@ public class DeviceController {
     }
 
     // Retorna um dispositivo específico pelo ID
+    @Operation(
+            summary = "Get the Device.",
+            description = "Endpoint responsible for getting device information searching for id.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Getting has been executed successfully.",
+                            content = @Content(mediaType = "application/json", schema = @Schema(
+                                implementation = Device.class))
+                    )
+            }
+    )
     @GetMapping("/{id}")
     public ResponseEntity<Device> getDeviceById(@PathVariable Long id) {
         Optional<Device> device = deviceService.findById(id);
@@ -32,6 +61,18 @@ public class DeviceController {
     }
 
     // Cria um novo dispositivo
+    @Operation(
+            summary = "Create a new Device.",
+            description = "Endpoint responsible for creating a new device, receiving a list of UserDeviceDataDTO to input the data.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "Device have been created..",
+                            content = @Content(mediaType = "application/json", schema = @Schema(
+                                implementation = Device.class))
+                    )
+            }
+    )
     @PostMapping
     public ResponseEntity<Device> createDevice(@RequestBody Device device) {
         Device savedDevice = deviceService.save(device);
@@ -39,6 +80,18 @@ public class DeviceController {
     }
 
     // Atualiza um dispositivo existente
+    @Operation(
+        summary = "Update an existing Device.",
+        description = "Endpoint responsible for updating an existing device by its ID.",
+        responses = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Device have been created..",
+                    content = @Content(mediaType = "application/json", schema = @Schema(
+                        implementation = Device.class))
+            )
+            }
+        )
     @PutMapping("/{id}")
     public ResponseEntity<Device> updateDevice(@PathVariable Long id, @RequestBody Device deviceDetails) {
         Device updatedDevice = deviceService.update(id, deviceDetails);
@@ -46,6 +99,16 @@ public class DeviceController {
     }
 
     // Exclui um dispositivo pelo ID
+    @Operation(
+        summary = "Delete a Device.",
+        description = "Endpoint responsible for deleting a device by its ID.",
+        responses = {
+                @ApiResponse(
+                        responseCode = "204",
+                        description = "Device deleted successfully. No content returned."
+                )
+        }
+    )
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteDevice(@PathVariable Long id) {
         deviceService.deleteById(id);
