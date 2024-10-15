e71f51e6338c323763a7dd48c00aa48c54635fe1
santosjhony12 <santosdesouzajhony@gmail.com>
2024-10-14 22:18:49 -0300

    feat: Upload file endpoint has been done with validations

--- src/main/java/br/fatec/bd4/service/RegistroService.java
+++ src/main/java/br/fatec/bd4/service/RegistroService.java
@@ -7,6 +7,7 @@ import br.fatec.bd4.repository.RegistroRepository;
 import br.fatec.bd4.web.dto.RegisterDTO;
 import br.fatec.bd4.web.dto.RegisterInputDTO;
 import br.fatec.bd4.web.dto.RegistersResponseDTO;
+import br.fatec.bd4.web.exception.EmptyFileException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
@@ -14,9 +15,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
@@ -49,9 +55,74 @@ public class RegistroService {
         registroRepository.deleteById(id);
     }
 
-
+    public boolean isValidLatitude(double latitude) {
+        return latitude >= -90 && latitude <= 90;
+    }
+    
+    public boolean isValidLongitude(double longitude) {
+        return longitude >= -180 && longitude <= 180;
+    }
+    
+    @Transactional
+    public void inputRegistersByUploadFile(MultipartFile file) {
+        if (file.isEmpty()){
+            throw new EmptyFileException("File is Empty");
+        }
+        try (BufferedReader buffer = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
+            String line;
+    
+            while((line = buffer.readLine()) != null) {
+                if (line.trim().isEmpty()) {
+                    continue;
+                }
+    
+                String[] fields = line.split(";");
+                System.out.println(fields[4]);
+    
+                if (fields.length < 5 || fields[1].trim().isEmpty() || fields[2].trim().isEmpty() || fields[3].trim().isEmpty() || fields[4].trim().isEmpty()) {
+                    continue;
+                }
+    
+                String fullName = fields[4].trim();
+                String createdAt = fields[1].trim();
+                double latitude = 0;
+                double longitude = 0;
+    
+                try {
+                    latitude = Double.parseDouble(fields[2].trim());
+                    longitude = Double.parseDouble(fields[3].trim());
+                    
+                } catch (NumberFormatException e) {
+                    continue;
+                }
+    
+                if (isValidLatitude(latitude) && isValidLongitude(longitude)) {
+                    LocalDateTime newDate = null;
+                    
+                    try {
+                        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
+                        newDate = LocalDateTime.parse(createdAt, formatter);
+                    } catch (DateTimeParseException e) {
+                        continue;
+                    }
+    
+                    if(newDate != null) {
+                        Local local = localService.findByLatitudeAndLongitude(new Local(fields[6], latitude, longitude));
+    
+                        Usuario usuario = usuarioService.findByNameAndCreate(fullName);
+    
+                        registroRepository.save(new Registro(newDate, usuario, local));
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Erro ao processar o arquivo CSV", e);
+        }
+    }
+    
     @Transactional
     public void inputRegisters(List<RegisterInputDTO> registers) {
+        
         for(RegisterInputDTO register : registers){
             if(register.fullName()!= null){
                 if(!register.fullName().equals("") && register.latitude() != null && register.longitude() != null && !register.createdAt().equals("")){
---
+++ src/main/java/br/fatec/bd4/web/exception/EmptyFileException.java
@@ -0,0 +1,10 @@
+package br.fatec.bd4.web.exception;
+
+public class EmptyFileException extends RuntimeException{
+
+    public EmptyFileException(String message){
+        super(message);
+    }
+
+
+}
