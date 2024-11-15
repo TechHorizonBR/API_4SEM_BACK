f0d7b893dfc9c195f2f6fb7ba7bf15d9d7a1061f
santosjhony12 <santosdesouzajhony@gmail.com>
2024-11-14 22:11:43 -0300

    feat: added class audit

--- src/main/java/br/fatec/bd4/entity/UserSys.java
+++ src/main/java/br/fatec/bd4/entity/UserSys.java
@@ -1,10 +1,18 @@
 package br.fatec.bd4.entity;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import br.fatec.bd4.entity.Enum.Role;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
@@ -22,6 +30,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class UserSys implements Serializable{
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -38,5 +47,21 @@ public class UserSys implements Serializable{
     @Enumerated(EnumType.STRING)
     @Column(name = "user_role", nullable = false, length = 25)
     private Role role = Role.ROLE_CLIENTE;
+
+    @CreatedDate
+    @Column(name = "data_criacao")
+    private LocalDateTime dataCriacao;
+
+    @LastModifiedDate
+    @Column(name = "data_modificacao")
+    private LocalDateTime dataModificacao;
+
+    @CreatedBy
+    @Column(name = "criado_por")
+    private String criadoPor;
+
+    @LastModifiedBy
+    @Column(name = "modificado_por")
+    private String modificadoPor;
  
 }
--- src/main/java/br/fatec/bd4/web/dto/UserSysResponseDTO.java
+++ src/main/java/br/fatec/bd4/web/dto/UserSysResponseDTO.java
@@ -1,15 +1,15 @@
 package br.fatec.bd4.web.dto;
 
+import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import br.fatec.bd4.entity.UserSys;
 import br.fatec.bd4.entity.Enum.Role;
 
-public record UserSysResponseDTO(Long id, String name, String username, Role role) {
+public record UserSysResponseDTO(Long id, String name, String username, Role role, String createdAt, String modifiedAt, String createdBy, String modifiedBy) {
     public static UserSysResponseDTO toUserResponseDTO(UserSys user){
-        return new UserSysResponseDTO(user.getId(), user.getName(), user.getUsername(), user.getRole());
+        return new UserSysResponseDTO(user.getId(), user.getName(), user.getUsername(), user.getRole(), String.valueOf(user.getDataCriacao()), String.valueOf(user.getDataModificacao()), user.getCriadoPor(), user.getModificadoPor());
     }
 
     public static UserSys toUserSys(UserSysUpdateDTO user){
